2d5fc6a6254216a7d95272cb85f4fbd0c8edccff
skalyan22 <48895749+skalyan22@users.noreply.github.com>
2020-07-22 16:26:07 -0400

    added all routes for each page

--- CodeLabs_Spyfall-master/app2.js
+++ CodeLabs_Spyfall-master/app2.js
@@ -13,7 +13,31 @@ app.set('view engine', 'handlebars');
 app.use(express.static(__dirname + "/public"));
 
 app.get('/', (request, response) => {
-    response.render('home');
+    response.render('index.handlebars');
+});
+
+app.get('/home', (request, response) => {
+    response.render('home.handlebars');
+});
+
+app.get('/host', (request, response) => {
+    response.render('host.handlebars');
+});
+
+app.get('/join', (request, response) => {
+    response.render('join.handlebars');
+});
+
+app.get('/game', (request, response) => {
+    response.render('game.handlebars');
+});
+
+app.get('/game-over', (request, response) => {
+    response.render('game-over.handlebars');
+});
+
+app.get('/aboutus', (request, response) => {
+    response.render('aboutus.handlebars');
 });
 
 app.listen(PORT, () => {
